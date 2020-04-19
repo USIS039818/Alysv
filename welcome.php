8536f0b463011ab44cac757d203f7c88bf6ab0df
Roberto Aly Garcia Ramirez <60438971+USIS039818@users.noreply.github.com>
2020-04-19 17:36:08 -0600

    Add files via upload
    Integrantes Equipo
    Gerson Alfredo Meléndez Núñez
    Roberto Aly García Ramírez
    Alexia Nahomy Cortez Rodríguez
    Héctor Hernán García Membreño

---
+++ welcome.php
@@ -0,0 +1,38 @@
+<?php
+// Initialize the session
+session_start();
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>bienvenido</title>
+    <link rel="stylesheet" href="index.html">
+
+   
+</head>
+<body>
+    <div class="page-header">
+        <h1>Hola, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Bienvenid@ a nuestro sitio.</h1>
+        
+    </div>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Cambia tu contraseña</a>
+        <a href="logout.php" class="btn btn-danger">Cierra la sesión</a>
+        <a href="index.html" class="btn btn-form">formularios</a>
+        </p>
+<p>
+       <img src="images/jp.png" width="500" ></a>
+        
+</p>
+
+
+</body>
+</html>
