b6dbe6d673504ca17a1f6a6e5abd18846f8c3a05
Roberto Aly Garcia Ramirez <60438971+USIS039818@users.noreply.github.com>
2020-05-24 14:19:07 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+		<link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.css">
+		<link rel="stylesheet" type="text/css" href="estilos/estilos.css">
+</head>
+<body background="images/tigre.jpg" style="background-attachment: fixed" >
+	<center><div class="tit"><h1 style="color:white;">Inicio de sesión</h1>
+	<br>
+		<center><div class="Ingreso">
+
+	<table border="0" align="center" valign="middle">
+		<tr>
+		<td rowspan=2>
+		<form action="validar.php" method="post">
+
+		<table border="0">
+
+		<tr><td><label style="font-size: 14pt"><b style="color:white;">Correo: </b></label></td>
+			<td width=80><input class="form-group has-success" style="border-radius:15px;" type="text" name="mail"></td></tr>
+		<tr><td><label style="font-size: 14pt"><b style="color:white;">Contraseña: </b></label></td>
+			<td witdh=80><input style="border-radius:15px;" type="password" name="pass"></td></tr>
+		<tr><td></td>
+			<td width=80 align=center><input class="btn btn-primary" type="submit" value="Aceptar"></td>
+			</tr></tr></table>
+		</form>
+<br>
+<br>
+<br>
+<p style="color:white;">¿No tienes una cuenta? <a href="regis.php">Regístrate ahora</a>.</p>
+
+
+	
+</body>
+</html>
---
+++ mapa.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mapa</title>
+</head>
+<body background="images/aniamles.jpg">
+
+		<h3 class="nav">
+			<li class=""><a href="index2.php"><font color="white">Principal</font></a></li>
+			<li class=""><a href="index.html"><font color="white">formularios</font></a></li>
+        </h3>
+		
+
+    <iframe src="https://www.google.com/maps/d/embed?mid=1TSXRrs0CU1zI09T4vYbB4f5Zcya2p0aq" width="1000" height="700"></iframe>
+        
+</>
+</html>
