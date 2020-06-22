b50fb466de723ddf96c613005942f5da80fe734e
Roberto Aly Garcia Ramirez <60438971+USIS039818@users.noreply.github.com>
2020-06-21 21:56:22 -0600

    Add files via upload

---
+++ private/config/config.php
@@ -0,0 +1,8 @@
+<?php 
+/**
+ * @author Gerson <usis020818@ugb.edu.sv>
+ * Importamos la clase principal de conexion
+ */
+include('../../Conexion/DB.php');
+$conexion = new DB('localhost','root','','db_sistema_peliculas');
+?>
